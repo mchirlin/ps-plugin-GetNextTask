162c04ee79995f5c85e3c407d7ec84bd6b3250fb
mchirlin <michael.chirlin@appian.com>
2014-03-10 23:43:41 +1100

    Updating Stubs

--- src/java/com/appiancorp/plugins/servlet/getnexttask/GetNextTaskServlet.java
+++ src/java/com/appiancorp/plugins/servlet/getnexttask/GetNextTaskServlet.java
@@ -1,22 +1,11 @@
 package com.appiancorp.plugins.servlet.getnexttask;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Properties;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import javax.servlet.ServletException;
-
 
 import org.apache.log4j.Logger;
 
@@ -29,18 +18,15 @@ public class GetNextTaskServlet extends HttpServlet {
 
   private static final long serialVersionUID = 9169350625721783901L;
   private static final Logger LOG = Logger.getLogger(GetNextTaskServlet.class);
-  
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,
-    IOException {
 
-    return;
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
+	  
   }
   
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    
-  }  
-
+    throws ServletException  {
+	  
+  }
 }
